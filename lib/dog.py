42e8f26b2f102852b07cc69c6f6d8b4d90b1aef4
mgenge254 <jeremiah.orodi@student.moringaschool.com>
2025-02-27 22:09:17 +0300

    newcommit

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python3
+
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+
+Rex = Dog("Rex", "Rotweilar" )
+print(f"My dog is called {Rex.name} and it's a {Rex.breed}")
+
+Snoopy = Dog("Snoopy")
+print(f"My dog is called {Snoopy.name} and it's a {Snoopy.breed}")
+        
